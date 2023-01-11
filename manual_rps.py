dd0b45cf97054b09f28c61aceab14ffc6a6b1cae
mungyeom <gyeomun.kim@gmail.com>
2023-01-11 14:53:08 +0000

    fourth commit

--- manual_rps.py
+++ manual_rps.py
@@ -11,6 +11,20 @@ def get_user_choice():
     user = input('rock, scissors, paper: ')
     return user
 
+def get_winner(user, com):
+    
+    if user == com: 
+        print("It is a tie!")
+    elif user == 'rock' and com == 'paper':
+        print("You lost")
+    elif user == 'scissors' and com == 'rock':
+        print("You lost")
+    elif user == 'paper' and com == 'scissors':
+        print("You lost")
+    else:
+        print("You won!")
+    
 
-
-
+com = get_computer_choice()
+user = get_user_choice()
+get_winner(user, com)
